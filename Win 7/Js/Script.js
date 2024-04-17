fd177e92f6762bbff0e1f09ae689224b57363d1c
CHIRAG TYAGI <69377702+chiragtyagi86@users.noreply.github.com>
2024-04-17 19:26:46 +0530

    Explorer edits

--- Win 7/Js/Script.js
+++ Win 7/Js/Script.js
@@ -1,13 +1,23 @@
+const showButton = document.querySelector("#showSectionButton");
+const hiddenSection = document.querySelector("#hiddenSection");
 
-const showButton = document.getElementById('showSectionButton');
-const hiddenSection = document.getElementById('hiddenSection');
-
-
-showButton.addEventListener('click', function() {
-
-    if (hiddenSection.style.opacity === '0') {
-        hiddenSection.style.opacity = "1";
-    } else {
-        hiddenSection.style.opacity = "0";
-    }
+showButton.addEventListener("click", function () {
+  if (hiddenSection.style.opacity === "0") {
+    hiddenSection.style.opacity = "1";
+  } else {
+    hiddenSection.style.opacity = "0";
+  }
 });
+
+function close(){
+
+  const explorer = document.querySelector("#Explorer");
+  
+  if(explorer.style.opacity === "flex"){
+    explorer.style.opacity = "none";
+  }
+  else{
+    explorer.style.opacity = "1";
+  }
+  
+  }
